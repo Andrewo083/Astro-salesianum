7ffc326ab6a3dec08228276f652d91ec2c1e451e
RUBEN NIETO <rn1354491@pae.ues.edu.sv>
2023-07-04 22:45:02 -0600

    FINAL PHP REGISTRO JOURNALIST

--- src/PHP/CRUD_journalist.php
+++ src/PHP/CRUD_journalist.php
@@ -1,3 +1,38 @@
 <?php 
 
+$conexion = mysqli_connect('localhost', 'root', '','astrodb')or die(mysqli_error($mysqli));
+
+diference($conexion);
+
+function diference($conexion){
+
+    if(isset($_POST['Enviar'])){
+        insertar($conexion);
+    }
+    
+}
+
+function insertar($conexion){
+
+    $email = $_POST['email'];
+    //REVISION
+
+    $presql = "SELECT * FROM `reporter` WHERE `Email` = '$email'";
+    mysqli_query($conexion, $presql);
+    if ($presql == true){
+ echo "LO SIENTO ESTE CORREO YA ESTA REGISTRADO";
+    }else{
+        echo "no se puede";
+    }
+    $password = $_POST['password'];
+    $name = $_POST['name'];
+    $lastname = $_POST['lastname'];
+    $phonenumber = $_POST['phonenumber'];
+    $ROL = 2;
+
+    $sql = "INSERT INTO `reporter`(`Email`, `Password`, `Name`, `LastName`, `PhoneNumber`, `ROL`) VALUES ('$email','$password','$name','$lastname','$phonenumber','$ROL')";
+
+    mysqli_query($conexion, $sql);
+
+}
 ?>
