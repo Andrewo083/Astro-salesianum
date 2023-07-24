434e664acab0872c6947a1246f2a99a40b05369d
Cyberking23 <102163019+Cyberking23@users.noreply.github.com>
2023-07-23 22:01:25 -0600

    Aministrador
    Trabajo('No concluido)

--- src/tableusu.php
+++ src/tableusu.php
@@ -81,7 +81,7 @@ $query = mysqli_query($conexion, $sql);
                       </div>
                     </div>
                     <div class="mt-4 mr-0 mb-0 ml-0 pt-0 pr-0 pb-0 pl-14 flex items-center sm:space-x-6 sm:pl-0 sm:mt-0">
-                      <a href="" class="bg-gray-900 pt-2 pr-6 pb-2 pl-6 text-lg font-medium text-gray-100 transition-all
+                      <a href="./AdministradorUptdate.php?ID=<?php echo $row['Email']; ?>" class="bg-gray-900 pt-2 pr-6 pb-2 pl-6 text-lg font-medium text-gray-100 transition-all
                           duration-200 hover:bg-gray-700 rounded-lg">Edit</a>
                       <a href="./PHP/DeleteReportero.php?ID=<?php echo $row['Email'] ?> " class="bg-red-800 pt-2 pr-6 pb-2 pl-6 text-lg font-medium text-gray-100 transition-all
                           duration-200 hover:bg-red-700 rounded-lg">Delete</a>
