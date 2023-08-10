8912396f1077eb675ce079b0bffdd75dcbdffbfd
RUBEN NIETO <rn1354491@pae.ues.edu.sv>
2023-08-09 22:13:51 -0600

    e

--- src/NewsMain.php
+++ src/NewsMain.php
@@ -114,7 +114,22 @@ while ($row = mysqli_fetch_array($result)) {
             <div class="flex rounded-md border border-slate-200">
               <div class="flex-1 p-10">
 
-                <p class="mt-2 text-slate-500"><?php echo $row['BodyFour']?></p>
+                <p class="mt-2 text-slate-500"><?php echo $row['BodyFour']?><br><br><b>Escrito por</b>
+              
+              <?php
+              $email = $row['id_reporter'];
+              $QueryReport = "SELECT * FROM `reporter` WHERE `Email` = '$email'";
+              $ResultReport = mysqli_query($conexion, $QueryReport);
+              while ($RowReport = mysqli_fetch_array($ResultReport)) {
+                echo "@".$RowReport['Name']." ".$RowReport['LastName'];
+              }
+              
+              ?>
+              
+              
+              
+              
+              </p>
 
               </div>
 
@@ -137,47 +152,42 @@ while ($row = mysqli_fetch_array($result)) {
             <div class="container px-5  mx-auto">
               <div class="flex flex-wrap -m-4">
                 <div class="p-4 md:w-1/3">
+<?php 
+$Category = $row['Category'];
+$id_news = $row['id_news'];
+                $QueryRecom = "SELECT * FROM `news` WHERE `Category` = '$Category' /*AND `id_news` != '$id_news'*/";
+$ResultRecom = mysqli_query($conexion, $QueryRecom);
+while ($RowQuery = mysqli_fetch_array($ResultRecom)) {
+
+
+
+  $contenido = $RowQuery['BodyOne']; //  aquí tienes el contenido de la base de datos
+  $numero_caracteres = 150;
+  $contenido_recortado = substr($contenido, 0, $numero_caracteres);
+
+
+?>
+                <!--INICIO RECOMENDACION-->
                   <div class="h-full rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
-                    <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="https://images.unsplash.com/photo-1618172193622-ae2d025f4032?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="blog">
+                    <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="../img/<?php echo $RowQuery['main_image'] ?>" alt="blog">
                     <div class="p-6">
-                      <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY-1</h2>
-                      <h1 class="title-font text-lg font-medium text-gray-600 mb-3">The Catalyzer</h1>
-                      <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                      <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"><?php echo $RowQuery['Category']?></h2>
+                      <h1 class="title-font text-lg font-medium text-gray-600 mb-3"><?php echo $RowQuery['headline']?></h1>
+                      <p class="leading-relaxed mb-3"><?php echo $contenido_recortado; ?>...</p>
                       <div class="flex items-center flex-wrap ">
-                        <button class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">Learn more</button>
+                        <a href="./NewsMain.php?new=<?php echo $RowQuery['id_news'];?>" class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">leer mas</a>
 
                       </div>
                     </div>
                   </div>
                 </div>
-                <div class="p-4 md:w-1/3">
-                  <div class="h-full rounded-xl shadow-cla-violate bg-gradient-to-r from-pink-50 to-red-50 overflow-hidden">
-                    <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="https://images.unsplash.com/photo-1624628639856-100bf817fd35?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8M2QlMjBpbWFnZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" alt="blog">
-                    <div class="p-6">
-                      <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY-1</h2>
-                      <h1 class="title-font text-lg font-medium text-gray-600 mb-3">The Catalyzer</h1>
-                      <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-                      <div class="flex items-center flex-wrap ">
-                        <button class="bg-gradient-to-r from-orange-300 to-amber-400 hover:scale-105 drop-shadow-md shadow-cla-violate px-4 py-1 rounded-lg">Learn more</button>
+                  <!--FIN RECOMENDACION-->
+               <?php } ?>
 
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="p-4 md:w-1/3">
-                  <div class="h-full rounded-xl shadow-cla-pink bg-gradient-to-r from-fuchsia-50 to-pink-50 overflow-hidden">
-                    <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="https://images.unsplash.com/photo-1631700611307-37dbcb89ef7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60" alt="blog">
-                    <div class="p-6">
-                      <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY-1</h2>
-                      <h1 class="title-font text-lg font-medium text-gray-600 mb-3">The Catalyzer</h1>
-                      <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-                      <div class="flex items-center flex-wrap ">
-                        <button class="bg-gradient-to-r from-fuchsia-300 to-pink-400 hover:scale-105  shadow-cla-blue px-4 py-1 rounded-lg">Learn more</button>
 
-                      </div>
-                    </div>
-                  </div>
-                </div>
+
+
+
               </div>
             </div>
           </section>
