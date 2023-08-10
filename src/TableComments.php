c001ad0282c29c5008c066acafec95cb584110c2
Andrewo083 <104947462+Andrewo083@users.noreply.github.com>
2023-08-10 15:13:26 -0600

    yaesta

--- src/TableComments.php
+++ src/TableComments.php
@@ -39,37 +39,56 @@
 </head>
 <body  class="w-full h-screen bg-no-repeat bg-cover"
 style="background-image: url('../img/bg_4.jpg');">
-    <nav class=" py-6 relative items-center ">
-        <div class="container mx-auto flex px-8 xl:px-0 ">
-          <div class="flex flex-grow">
-            <a href="">
-       
+<div>
+        <section class="relative mx-auto">
+            <!-- navbar -->
+            <nav class="flex justify-between bg-black text-white ">
+            <div class="px-5 xl:px-12 py-6 flex w-full items-center">
+                <a class="text-3xl font-bold font-heading" href="../src/maindex.html">
+                <!-- <img class="h-9" src="logo.png" alt="logo"> -->
+              Astro Salesianum
+                </a>
+                <!-- Nav Links -->
+                <ul class="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
+                <li><a  href="./newindex.php" class="hover:text-gray-200" href="#">Home</a></li>
+                <li><a href="./ChalecoIndex.php" class="hover:text-gray-200" href="#">Chaleco</a></li>
+                <li><a href="./Ricalindex.php" class="hover:text-gray-200" href="#">Ricaldonde</a></li>
+                <li><a href="./DonBoscoIndex.php" class="hover:text-gray-200" href="#">Don bosco</a></li>
+                <li><a  href="./MariaIndex.php" class="hover:text-gray-200" href="#">Maria axuliadora</a></li>
+                </ul>
+                <!-- Header Icons -->
+                <div class="hidden xl:flex items-center space-x-5 ">
+                <a class="hover:text-gray-200" href="#">
+                    
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                    </svg>
+<a>
+                <a class="flex items-center hover:text-gray-200" href="#">
+                    
+                </a>
+                <!-- Sign In / Register      -->
+                <a class="flex items-center hover:text-gray-200" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                </a>
+                
+                </div>
+            </div>
+            <!-- Responsive navbar -->
+            <a class="xl:hidden flex mr-6 items-center" href="#">
+                
+                
             </a>
-            <h1 class="flex items-center m-2 font-bold text-3xl font-heading  text-gray-500">
-              Astro alesianum
-            </h1>
-          </div>
-          <div class="flex lg:hidden">
-          <img src="../img/menu.png" onclick="openMenu();">
-      </div>
-          <div id="menu" class="lg:flex hidden flex-grow justify-between items-center absolute lg:relative lg:top-0 top-20 ">
-            <div>
-              <a href="#" class="text-white hover:text-black lg:mr-7 font-semibold ">Periodistas</a>
-              <a href="#" class="text-white hover:text-black lg:mr-7 font-semibold ">services</a>
-              <a href="#" class="text-white  hover:text-black lg:mr-7 font-semibold ">mision</a>
-              <a href="#" class="text-white hover:text-black font-semibold ">other</a>
-            </div>
-            <div>
-        
-              <a
-                href="../src/login.html"
-                class="bg-black text-white p-3 m-3 font-semibold  cursor-pointer rounded hover:bg-white hover:text-black ease-in duration-200 flex-col "
-                >Log out</a
-              >
-            </div>
-          </div>
+            <a class="navbar-burger self-center mr-12 xl:hidden" href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    
+                </svg>
+            </a>
+            </nav>
+            
+        </section>
         </div>
-    <!-- finmenu-->
 
     <!-- tabla-->
 
--- src/tableusu.php
+++ src/tableusu.php
@@ -22,34 +22,56 @@ $query = mysqli_query($conexion, $sql);
   <title>Document</title>
 </head>
 
-<body class="w-full h-screen bg-no-repeat bg-cover" style="background-image: url('../img/back.jpg');">
-  <nav class=" py-6 relative items-center ">
-    <div class="container mx-auto flex px-8 xl:px-0 ">
-      <div class="flex flex-grow">
-        <a href="">
+<body class="w-full h-screen bg-no-repeat bg-cover" style="background-image: url('../img/bg_4.jpg');">
+<div>
+        <section class="relative mx-auto">
+            <!-- navbar -->
+            <nav class="flex justify-between bg-black text-white ">
+            <div class="px-5 xl:px-12 py-6 flex w-full items-center">
+                <a class="text-3xl font-bold font-heading" href="../src/maindex.html">
+                <!-- <img class="h-9" src="logo.png" alt="logo"> -->
+              Astro Salesianum
+                </a>
+                <!-- Nav Links -->
+                <ul class="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
+                <li><a  href="./newindex.php" class="hover:text-gray-200" href="#">Home</a></li>
+                <li><a href="./ChalecoIndex.php" class="hover:text-gray-200" href="#">Chaleco</a></li>
+                <li><a href="./Ricalindex.php" class="hover:text-gray-200" href="#">Ricaldonde</a></li>
+                <li><a href="./DonBoscoIndex.php" class="hover:text-gray-200" href="#">Don bosco</a></li>
+                <li><a  href="./MariaIndex.php" class="hover:text-gray-200" href="#">Maria axuliadora</a></li>
+                </ul>
+                <!-- Header Icons -->
+                <div class="hidden xl:flex items-center space-x-5 ">
+                <a class="hover:text-gray-200" href="#">
+                    
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                    </svg>
+<a>
+                <a class="flex items-center hover:text-gray-200" href="#">
+                    
+                </a>
+                <!-- Sign In / Register      -->
+                <a class="flex items-font-bold text-white" href="#">
+                  Log out
+                </a>
+                
+                </div>
+            </div>
+            <!-- Responsive navbar -->
+            <a class="xl:hidden flex mr-6 items-center" href="#">
+                
+                
+            </a>
+            <a class="navbar-burger self-center mr-12 xl:hidden" href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    
+                </svg>
+            </a>
+            </nav>
+            
+        </section>
+        </div>
 
-        </a>
-        <h1 class="flex items-center m-2 font-bold text-3xl font-heading  text-white">
-          Astro alesianum
-        </h1>
-      </div>
-      <div class="flex lg:hidden">
-        <img src="../img/menu.png" onclick="openMenu();">
-      </div>
-      <div id="menu" class="lg:flex hidden flex-grow justify-between items-center absolute lg:relative lg:top-0 top-20 ">
-        <div>
-          <a href="./Administrador.php" class="text-white hover:text-black lg:mr-7 font-semibold ">Creacion de Reporteros</a>
-          <a href="../src/AdminNewsTable.php" class="text-white hover:text-black lg:mr-7 font-semibold ">Publicaciones</a>
-          <a href="#" class="text-white  hover:text-black lg:mr-7 font-semibold ">mision</a>
-          <a href="#" class="text-white hover:text-black font-semibold ">other</a>
-        </div>
-        <div>
-          <a href="#" class="  text-white font-semibold p-3 m-3 cursor-pointer rounded hover:bg-white hover:text-black ease-in duration-200">Iniciar sesion</a>
-          <a href="../src/login.html" class="bg-black text-white p-3 m-3 font-semibold  cursor-pointer rounded hover:bg-white hover:text-black ease-in duration-200 flex-col ">Registrate</a>
-        </div>
-      </div>
-    </div>
-    <!-- finmenu-->
 
     <!-- tabla-->
     <div class=" pt-12 pr-0 pb-12 pl-0 mt-0 mr-auto mb-0 ml-auto sm:py-16 lg:py-20">
@@ -57,9 +79,17 @@ $query = mysqli_query($conexion, $sql);
         <div class="pt-0 pr-4 pb-0 pl-4 mt-0 mr-auto mb-0 ml-auto max-w-4xl sm:px-6 lg:px-8">
           <div class="pt-0 pr-4 pb-0 pl-4 mt-0 mr-auto mb-0 ml-auto sm:flex sm:items-center sm:justify-between">
             <div>
-              <p class="text-xl font-bold text-white">Adminitracion de cuentas</p>
-              <p class="text-sm mt-1 mr-0 mb-0 ml-0 font-semi-bold text-gray-300">Aqui podras editar los datos del reportero o en dado caso, eliminarlo.</p>
+              <p class="text-xl font-bold text-black">Adminitracion de cuentas</p>
+              <p class="text-sm mt-1 mr-0 mb-0 ml-0 font-semi-bold text-black">Aqui podras editar los datos del reportero o en dado caso, eliminarlo.</p>
             </div>
+            <div class="mt-4 mr-0 mb-0 ml-0 sm:mt-0">
+                <p class="sr-only">Search Position</p>
+                <div class="relative">
+                
+                  <button href="" class="bg-gray-800 pt-2 pr-6 pb-2 pl-6 text-lg font-medium text-gray-100 transition-all
+                          duration-200 hover:bg-gray-700 rounded-lg">Agregar Reporteros</button>
+                </div>
+              </div>
           </div>
           <div class="shadow-xl mt-8 mr-0 mb-0 ml-0 pt-4 pr-10 pb-4 pl-10 flow-root rounded-lg sm:py-2">
 
