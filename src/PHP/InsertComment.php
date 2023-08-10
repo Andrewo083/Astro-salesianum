07d227525699dfb96d5030111d3752e580fb34ad
RUBEN NIETO <rn1354491@pae.ues.edu.sv>
2023-08-09 22:36:07 -0600

    e

--- src/PHP/InsertComment.php
+++ src/PHP/InsertComment.php
@@ -15,6 +15,7 @@ if(isset($_SESSION['Email'])){
     }
 
     $_SESSION['Name'];
+
     $Comment = $_POST['Comment'];
     $id_user = $_SESSION['Email'];  // Asegúrate de obtener el valor correcto del usuario
     $id_new = $_GET['new'];    // Asegúrate de obtener el valor correcto de la noticia
@@ -32,7 +33,7 @@ if(isset($_SESSION['Email'])){
         $sql = "INSERT INTO `comments`(`id_user`, `id_new`, `comment`, `date`) VALUES ('$id_user','$id_new','$Comment','$date')";
         if ($conexion->query($sql)) {
             echo "Datos insertados correctamente";
-            // header('Refresh: 2; URL=http://localhost/Astro-salesianum/src/tableusu.php');
+         header('Refresh: 2; URL=http://localhost/Astro-salesianum/src/NewsMain.php?new='.urlencode($id_new));
         } else {
             echo "Error: " . $conexion->error;
         }
