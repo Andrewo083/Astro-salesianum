6180249486adddaa0435e64b3dfccde748dcdfa3
Cyberking23 <102163019+Cyberking23@users.noreply.github.com>
2023-08-18 20:20:10 -0600

    DELETE
    ghjkl

--- src/Funciones/DeleteComments.php
+++ src/Funciones/DeleteComments.php
@@ -1,29 +1,23 @@
 <?php
-
-
-
 include('conexion.php');
 
-$conexion = new mysqli($host,$user,$password,$bd);
+$conexion = new mysqli($host, $user, $password, $bd);
 
-if(!$conexion){
-    die("Error en la conexion".mysqli_connect_error());
+if (!$conexion) {
+    die("Error en la conexion" . mysqli_connect_errno());
 }
 
-$ID=$_GET['ID'];
-$State = "Inactive";
+$ID = $_GET['ID'];
 
-$sql="UPDATE `comments` SET `State`='$State' WHERE id_comment = $ID";
+$SQL = "DELETE FROM `comments` WHERE id_comment = $ID";
 
-if(mysqli_query($conexion,$sql)){
-    echo "Registro eliminado exitosamente";
-    header("Refresh:2; http://localhost/Astro-salesianum/src/TableComments.php");
-}else{
-    echo "Error al tratar de eliminar datos";
+if ($conexion->query($SQL)) {
+    echo "Comentario Eliminado Exitosamente";
+    header('Refresh: 2; URL=http://localhost/Astro-salesianum/src/TableComments.php');
+} else {
+    echo "Ha ocurrido un error al eliminar el comentario: " . $conexion->error;
+    header('Refresh: 2; URL=http://localhost/Astro-salesianum/src/TableComments.php');
 }
 
-
-
-mysqli_close($conexion);
-
+$conexion->close();
 ?>
