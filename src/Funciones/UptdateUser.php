ac8edcec4af05338e96b9ba9e4e7d2a9baca47ac
Cyberking23 <merinoventura123@gmail.com>
2023-08-24 15:34:19 -0600

    foiads
    fghjklñ

--- src/Funciones/UptdateUser.php
+++ src/Funciones/UptdateUser.php
@@ -14,18 +14,46 @@ $PhoneNumber = $_POST['PhoneNumber'];
 $Password = $_POST['Password'];
 $Rol = 3;
 
-// Sanitizar y escapar el valor de Email para prevenir inyección de SQL
+// Sanitizar y escapar los valores para prevenir inyección de SQL
 $ID = mysqli_real_escape_string($conexion, $ID);
+$Name = mysqli_real_escape_string($conexion, $Name);
+$LastName = mysqli_real_escape_string($conexion, $LastName);
+$PhoneNumber = mysqli_real_escape_string($conexion, $PhoneNumber);
+$Password = mysqli_real_escape_string($conexion, $Password);
 
-$SQL = "UPDATE `user` SET `Name`='$Name',`LastName`='$LastName',`Password`='$Password',
-        `PhoneNumber`='$PhoneNumber',`ROL`='$Rol',`ProfileImage`='[value-7]' WHERE Email = '$ID'";
+// Verificar si hay algún cambio en los campos
+$SQL = "SELECT * FROM `user` WHERE Email = '$ID'";
+$result = mysqli_query($conexion, $SQL);
+$row = mysqli_fetch_assoc($result);
 
-// Ejecucion de la consulta
-if (mysqli_query($conexion, $SQL)) {
-    echo "Datos actualizados exitosamente";
-    header('Refresh: 2; URL=http://localhost/Astro-salesianum/src/ProfileUser.php');
+$updateNeeded = false;
+
+if ($row['Name'] != $Name ||
+    $row['LastName'] != $LastName ||
+    $row['PhoneNumber'] != $PhoneNumber ||
+    $row['Password'] != $Password) {
+    $updateNeeded = true;
+}
+
+if ($updateNeeded) {
+    $updateSQL = "UPDATE `user` SET `Name`='$Name',`LastName`='$LastName',`Password`='$Password',
+                  `PhoneNumber`='$PhoneNumber',`ROL`='$Rol',`ProfileImage`='[value-7]' WHERE Email = '$ID'";
+    
+    if (mysqli_query($conexion, $updateSQL)) {
+        echo "Datos actualizados exitosamente";
+        header('Refresh: 2; URL=http://localhost/Astro-salesianum/src/ProfileUser.php');
+    } else {
+        echo "Error al actualizar el registro";
+    }
 } else {
-    echo "Error al actualizar el registro";
+    echo "<script>
+            var noChanges = confirm('No hay cambios en los datos. ¿Deseas regresar a tu perfil?');
+            if (noChanges) {
+                window.location.href = 'http://localhost/Astro-salesianum/src/ProfileUser.php';
+            } else {
+                window.location.href = 'http://localhost/Astro-salesianum/src/ProfileUser.php'; // Cambiar la URL según sea necesario
+            }
+          </script>";
 }
 
 mysqli_close($conexion);
