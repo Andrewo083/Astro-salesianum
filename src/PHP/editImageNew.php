50d9a2adfd5db5b4bfca4c373ee1736ec03cbb54
RUBEN NIETO <134467171+NietoRuben@users.noreply.github.com>
2023-08-28 10:34:41 -0600

    Actualizar editImageNew.php

--- src/PHP/editImageNew.php
+++ src/PHP/editImageNew.php
@@ -7,21 +7,19 @@ if($_SESSION['ROL'] == 1){
     header("Location: ./Error.html");
 }else{
 
-$Email = $_SESSION['Email'];
-$Name = $_SESSION['Name'];
-$id_new = $_POST['id_new'];
-$headline =$_POST['headline'];
 
+$id_reporter = $_POST['id_reporter'];
+$headline =$_POST['headline'];
+$photographer = $_POST['photographer'];
 $id_news = $_POST['id_news'];
-$ROL = $_SESSION['ROL'];
+
 
 $carpet_images = "C:/xampp/htdocs/Astro-salesianum/img/";
 
 $imagen = $_FILES['imagen']['name'];
     $imagen_tmp = $_FILES['imagen']['tmp_name'];
 
-    $imagen =  $Email."-".$Name."-".$ROL.".png";
-
+        $imagen =  "$headline-$id_reporter-fotografo-$photographer.png";
 
     $urlPlusImage = $carpet_images.$imagen;
     move_uploaded_file($imagen_tmp, $urlPlusImage);
