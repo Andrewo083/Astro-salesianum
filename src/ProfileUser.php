20d2e17ee518ac9baba75986db357337ae6878f2
RUBEN NIETO <rn1354491@pae.ues.edu.sv>
2023-08-30 00:59:24 -0600

    stash merino

--- src/ProfileUser.php
+++ src/ProfileUser.php
@@ -47,7 +47,9 @@ if ($_SESSION['ROL'] != 3) {
       <link rel="stylesheet" href="./css/ProfileImageUser.css">
       <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/iconfont/material-icons.min.css" integrity="sha256-x8PYmLKD83R9T/sYmJn1j3is/chhJdySyhet/JuHnfY=" crossorigin="anonymous" />
-      <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
 
     </head>
 
