4cd1994241d5521e3ebefcc36d0b1546e9e0f364
Andrewo083 <souland4@gmail.com>
2023-08-29 23:23:36 -0600

    menus y ojo

--- src/welcomeAdmin.php
+++ src/welcomeAdmin.php
@@ -22,55 +22,69 @@ if(isset($_SESSION['ROL'])){
     <link rel="stylesheet" href="../Public/tailwind.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Astro Administrador </title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/iconfont/material-icons.min.css" integrity="sha256-x8PYmLKD83R9T/sYmJn1j3is/chhJdySyhet/JuHnfY=" crossorigin="anonymous" />
+
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>  <link rel="stylesheet" href="../Public/boton.css">
      <link rel="stylesheet" href="./css/Translate.css">
 </head>
 <body>
-    <div>
-        <section class="relative mx-auto">
-            <!-- navbar -->
-            <nav class="flex justify-between bg-black text-white ">
-            <div class="px-5 xl:px-12 py-6 flex w-full items-center">
-            <a href="./WelcomeAdmin.php">
-            <img src="../img/AstroFavicon.png" Alt="Imagen representativa de Noticias Astro Salesianum" width="45px"></a>
-                <a class="text-3xl font-bold font-heading" href="./welcomeAdmin.php">
-                    <!-- <img class="h-9" src="logo.png" alt="logo"> -->
-                  Astro <b>Administrador</b> 
-                    </a>
-                   
-                
-                    <!-- Nav Links -->
-                    <ul class="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
-                    <li><a href="./newindex.php" class="hover:text-gray-200" href="#">Home</a></li>
-            <li><a href="./welcomeAdmin.php" class="hover:text-gray-200">Administracion</a></li>
-            <li><a href="./AdminJourTable.php" class="hover:text-gray-200" >Periodistas</a></li>
-            <li><a href="./AdminNewsTable.php" class="hover:text-gray-200" href="#">Noticias</a></li>
-            <li><a href="./" class="hover:text-gray-200" href="#">Comentarios</a></li>
-                  
-                    </ul>
-                <!-- Header Icons -->
-                <div class="hidden xl:flex items-center space-x-5 ">
-            
-                    
-                    
-                
-                <!-- Sign In / Register      -->
-                <a
-                href="./PHP/Logout.php"
-                class="bg-black text-white p-3 m-3 font-semibold  cursor-pointer rounded hover:bg-white hover:text-black ease-in duration-200 flex-col "
-                >Cerrar Sesion</a
-              >
-                
-                </div>
-            </div>
-            <!-- Responsive navbar -->
-            
-            
-            </nav>
-            
-        </section>
-        </div>
+<nav class="flex items-center bg-gray-900 p-3 flex-wrap">
+      <a href="./newindex.php" class="p-2 mr-4 inline-flex items-center">
+    <img src="../img/AstroFavicon.png" alt="" class="h-8 w-8 mr-2">
+        <span class="text-xl text-white font-bold uppercase tracking-wide"
+          >Astrum Salesianum</span
+        >
+      </a>
+  
+      <button
+        class="text-white inline-flex p-3 hover:bg-gray-900 rounded lg:hidden ml-auto hover:text-white outline-none nav-toggler"
+        data-target="#navigation"
+      >
+        <i class="material-icons">menu</i>
+      </button>
+      <div
+        class="hidden top-navbar w-full lg:inline-flex lg:flex-grow lg:w-auto"
+        id="navigation"
+      >
+        <div
+          class="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto"
+        >
+          <a
+            href="./newindex.php"
+            class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-gray-900 hover:text-white"
+          >
+            <span  >Home</span>
+          </a>
+          <a
+            href="./AdminJourTable.php"
+            class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white  items-center justify-center hover:bg-gray-900 hover:text-white"
+          >
+            <span  >Periodistas</span>
+          </a>
+          <a
+            href="./AdminNewsTable.php"
+            class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white  items-center justify-center hover:bg-gray-900 hover:text-white"
+          >
+            <span>Noticias</span>
+          </a>
+          <a
+            href="./MariaIndex.php"
+            class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-gray-900 hover:text-white"
+          >
+            <span>Comentarios</span>
+          </a>
+          
+       
     
+        </div>
+        <div class="imagen-circular hidden lg:block" >
+        <!-- Aquí colocas la URL de la imagen -->
+                    <img src="../img/<?php echo $_SESSION['ProfileImage'] ?>" alt="Imagen Circular" class="h-8 w-8">
+                  </div>
+      </div>
+    </nav>
+
    
 	<div class=" bg-red-200 h-96 bg-no-repeat"style="background-image: url(../img/bg.2.jpeg)">
 	</div>
@@ -158,7 +172,7 @@ if(isset($_SESSION['ROL'])){
 
           <div class="flex flex-col mt-4 sm:flex-row sm:items-center sm:justify-center">
              
-
+          <div class = "group fixed bottom-0 right-0 p-2  flex items-end justify-end w-24 h-24 " id="google_translate_element">
               
           </div>
       </div>
@@ -170,7 +184,7 @@ if(isset($_SESSION['ROL'])){
 
           <div class="flex mt-3 -mx-2 sm:mt-0">
               <a href="#" class="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit">  
-                       <div class = "group fixed bottom-0 right-0 p-2  flex items-end justify-end w-24 h-24 " id="google_translate_element">
+                       
  </a>
 
               <a href="#" class="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit"> Privacy </a>
@@ -181,9 +195,24 @@ if(isset($_SESSION['ROL'])){
       </div>
   </div>
 </footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+<script>
+  $(document).ready(function() {
+  $(".nav-toggler").each(function(_, navToggler) {
+    var target = $(navToggler).data("target");
+    $(navToggler).on("click", function() {
+      $(target).animate({
+        height: "toggle"
+      });
+    });
+  });
+});
+
+</script>
+
 <script> 
 function googleTranslateElementInit() { new google.translate.TranslateElement({ pageLanguage: "es",includedLanguages: "en,es",}, "google_translate_element" ); } </script>
-<script src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 <script src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
 </body>
